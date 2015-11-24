43626e58555e14f2558a4a3d6b02c9d1b9a0eb62
Branci <branislav.smik@gmail.com>
2015-11-24 13:22:28 +0100

    commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPostsApp::Application.config.secret_key_base = '38f8d771fe5148e7091b7d7a034b253935c4b1a20adb43ae2dea2e427cabb20f0f10e372b579ffdf0ffc3215b70256fd64bd229486622cc83ff7c43010adc65e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsPostsApp::Application.config.session_store :cookie_store, key: '_rails_posts_app_session'
